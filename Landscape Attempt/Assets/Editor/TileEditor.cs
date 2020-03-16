0c2c13bf9b00bf51609ab223466b5f1ee89de43e
jcarroll <jrc.csus@gmail.com>
2020-03-16 13:27:08 -0600

    initial commit

---
+++ Landscape Attempt/Assets/Editor/TileEditor.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+class TileEditor : EditorWindow {
+    private Dictionary<string, List<string>> fileData;
+
+    [MenuItem("Window/TileEditor")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(TileEditor));
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Reload Assets"))
+        {
+            fileData = retrive_AssetsByType();
+        }
+
+        if (fileData != null)
+        {
+            foreach (var key in fileData.Keys)
+            {
+                //
+                foreach (var file in fileData[key])
+                {
+                    Debug.Log(file.Split('.')[0]);
+                    Texture2D myTexture = AssetPreview.GetAssetPreview(Resources.Load(file.Split('.')[0]));
+                    GUILayout.Button(myTexture, GUILayout.Width(95), GUILayout.Height(95));
+                }
+            }
+        }
+    }
+
+    void OnClick(SceneView scene)
+    {
+        Event e = Event.current;
+
+        if (e.type == EventType.MouseDown && e.button == 2)
+        {
+            Debug.Log("Middle Mouse was pressed");
+
+            Vector3 mousePos = e.mousePosition;
+            float ppp = EditorGUIUtility.pixelsPerPoint;
+            mousePos.y = scene.camera.pixelHeight - mousePos.y * ppp;
+            mousePos.x *= ppp;
+
+            Ray ray = scene.camera.ScreenPointToRay(mousePos);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Do something, ---Example---
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                go.transform.position = hit.point;
+                Debug.Log("Instantiated Primitive " + hit.point);
+            }
+            e.Use();
+        }
+    }
+    private Dictionary<string, List<string>> retrive_AssetsByType()
+    {
+        var assets = new Dictionary<string, List<string>> { };
+        var fileNames = new DirectoryInfo("Assets\\Resources");
+
+        foreach (string file in Directory.GetFiles("Assets\\Resources")) {
+            
+            var fileName = Path.GetFileName(file);
+            var ext = fileName.Split('.');
+            var type = fileName.Split('_');
+
+            Debug.Log($"{fileName} {ext} {type}");
+
+            if (ext[ext.Length - 1] == "meta")
+            {
+                // pass
+            }
+            else if (!assets.ContainsKey(type[0]))
+            {
+                assets.Add(type[0], new List<string> { fileName });
+            } else
+            {
+                assets[type[0]].Add(fileName);
+            }
+        }
+
+        return assets;
+    }
+}
---
+++ Landscape Attempt/Assets/TerrainBuilder.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainBuilder : MonoBehaviour
+{
+    public enum BuildStartType
+    {
+        CenterLinear,
+        CenterSpiral,
+        Random,
+        PlayerStart,
+        EdgesInward,
+        AllAtOnce
+    };
+
+    public int playFieldSizeX = 5;
+    public int playFieldSizeY = 5;
+    public GameObject gameCube = null;
+    public BuildStartType buildUsing;
+    private GameObject[,] land = null;
+    private Vector3 start;
+    private Transform floorDimension;
+    private float _adjustYFloor;
+
+    private float newX;
+    private float newZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Create the 2D array that will house the land objects for reference.
+        land = new GameObject[playFieldSizeX, playFieldSizeY];
+        // Mark starting location
+        start = new Vector3(0,0,0);
+        // Annoate the floor dimension
+        floorDimension = gameCube.transform;
+        _adjustYFloor = (floorDimension.localScale.y/2);
+        start.y = _adjustYFloor;
+
+        for (int y = 0; y < playFieldSizeY; y++)
+        {
+            for (int x = 0; x < playFieldSizeX; x++)
+            {
+                GameObject cube = Instantiate(gameCube);
+                cube.transform.SetParent(gameObject.transform);
+                placeNewFloor(cube, x, y);
+                land[x, y] = cube;
+            }
+        }
+
+        return;
+    }
+
+    void placeNewFloor(GameObject cube, float x, float y)
+    {
+        // Need to adjust cub down to the floor.
+        newX = 200*x;
+        newZ = 200*y;
+        cube.transform.position = new Vector3(-newX, -start.y, -newZ);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
